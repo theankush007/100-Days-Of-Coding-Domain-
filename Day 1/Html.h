8911dd5cba4a498966917decf06c87188d1ea03d
theankush007 <114766438+theankush007@users.noreply.github.com>
2022-10-05 23:49:42 +0530

    Create Html.h

---
+++ Day 1/Html.h
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Heading-Paragraph Document</title>
+</head>
+<body>
+    <h1>THis is heading 1</h1>
+    <h2>THis is heading 1</h2>
+    <h3>THis is heading 1</h3>
+    <h4>THis is heading 1</h4>
+    <h5>THis is heading 1</h5>
+    <h6>THis is heading 1</h6>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam quae commodi ex? Fugit eaque aut quis voluptas est sit blanditiis?</p>
+
+
+</body>
+</html>
